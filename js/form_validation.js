6eb2999ee292fa962b1f8c4a6b5091e36fcf711e
notbleh <yu.billy@gmail.com>
2016-12-26 14:41:03 +1030

    Update form_validation.js

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,7 +1,28 @@
-function validateForm() {
-    var x = document.forms["rsvp"]["name"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
+$(document).ready(function () {
+    $("#form").validate({
+        rules: {
+            "name": {
+                required: true,
+                minlength: 5
+            },
+            "email": {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            "name": {
+                required: "Please, enter a name"
+            },
+            "email": {
+                required: "Please, enter an email",
+                email: "Email is invalid"
+            }
+        },
+        submitHandler: function (form) { // for demo
+            alert('valid form submitted'); // for demo
+            return false; // for demo
+        }
+    });
+
+});
