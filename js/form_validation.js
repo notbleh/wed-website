00b6fb7c4d64dcd4a8a0ca5ed3cc23663577af4a
notbleh <yu.billy@gmail.com>
2016-12-29 22:57:09 +1030

    Update form_validation.js

--- js/form_validation.js
+++ js/form_validation.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
         // submitHandler: function (rsvpform) { // for demo
             // alert('valid form submitted'); // for demo
             // return false; // for demo
-        }
+        // }
     });
 
 });
