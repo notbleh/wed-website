db0bf471ba1e27d717185ca0951ee301695731fe
notbleh <yu.billy@gmail.com>
2017-01-01 18:07:19 +1030

    Update food_rsvp.js

--- js/food_rsvp.js
+++ js/food_rsvp.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  
   var guestdatefood = $('#guestdatefood');
     guestdatefood.hide();
 
@@ -13,4 +14,21 @@ $(document).ready(function() {
     $('#gnamedate').keyup(function () {
       $('#gnamedatedisp').text($(this).val());
     });
+  
+  var guest1food = $('#guest1food');
+    guest1food.hide();
+  
+  $('#gname1').keyup(function(){
+    if($(this).val() == ''){  
+      guest1food.hide();
+    }else{
+      guest1food.show();
+      }
+  });
+  
+  $('#gname1').keyup(function () {
+  	$('#gname1disp').text($(this).val());
+  });
+  
+  
 });
