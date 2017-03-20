f426898bb39d948f9c06d5f0370471f26fc4a20b
notbleh <yu.billy@gmail.com>
2017-03-20 23:02:44 +1100

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -55,14 +55,21 @@ $('.responsive-4').slick({
       }
     },
     {
-      breakpoint: 900,
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 486,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
