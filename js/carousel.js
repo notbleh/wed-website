bc8edb29dfce07d0555d5c54adaa166c90c33b37
notbleh <yu.billy@gmail.com>
2017-03-20 22:51:10 +1100

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -48,7 +48,7 @@ $('.responsive-4').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         infinite: true,
         dots: true
