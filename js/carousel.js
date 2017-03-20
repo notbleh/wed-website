87ee5035b34cf5b1311340f02884aa414cdd67e7
notbleh <yu.billy@gmail.com>
2017-03-20 22:42:54 +1100

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,42 @@
 $(document).ready(function(){
-$('.responsive').slick({
+$('.responsive-3').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  touchThreshold: 20,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.responsive-4').slick({
   arrows: false,
   dots: true,
   infinite: true,
