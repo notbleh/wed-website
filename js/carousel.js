3b2c921e66e585f1100c7d6e5977bae3aa6d6efe
notbleh <yu.billy@gmail.com>
2017-06-08 16:32:19 +1000

    Create carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -34,6 +34,7 @@ $('.responsive-3').slick({
     }
   ]
 });
+
 $('.responsive-4').slick({
   arrows: false,
   dots: true,
@@ -75,4 +76,29 @@ $('.responsive-4').slick({
     }
   ]
 });
+});
+
+
+function setSlideVisibility() {
+  //Find the visible slides i.e. where aria-hidden="false"
+  var visibleSlides = $('.slick-slide[aria-hidden="false"]');
+  //Make sure all of the visible slides have an opacity of 1
+  $(visibleSlides).each(function() {
+    $(this).css('opacity', 1);
+  });
+  //Set the opacity of the first and last partial slides.
+  $(visibleSlides).first().prev().css('opacity', 0);
+  $(visibleSlides).last().next().css('opacity', 0);
+}
+
+$(setSlideVisibility());
+
+$('.slider').on('beforeChange', function() {
+  $('.slick-slide').each(function() {
+  	$(this).css('opacity', 1);
+  });
+});
+
+$('.slider').on('afterChange', function() {
+  setSlideVisibility();
 });
